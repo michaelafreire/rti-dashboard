73324cfdb4d906c19dafc2f19a83024b1180dbd5
Michaela Freire <michaela.freiregriffith@gmail.com>
2026-02-20 16:28:57 +0100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme/theme.ts';
+import CssBaseline from '@mui/material/CssBaseline';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
